f3aa2a1627431cafac66d84c9639a6e763e889cb
MoamenZyan <moamenzyan20@gmail.com>
2024-05-08 01:02:56 +0300

    files

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""My Script"""
+
+from sys import argv
+import requests
+
+
+def top_ten(subreddit):
+    """docs"""
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
